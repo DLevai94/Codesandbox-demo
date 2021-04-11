eff47e3dd1e2de77e1acb4d5dce17b45e53f924b
Dávid Lévai <david@davidlevai.com>
2021-04-11 09:59:25 +0200

    Add environment config

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
+      Hello World.{" "}
       <Link href="/about">
         <a>About</a>
       </Link>
+      <p>{`${process.env.NEXT_PUBLIC_ENV} environment`}</p>
     </div>
-  )
+  );
 }
